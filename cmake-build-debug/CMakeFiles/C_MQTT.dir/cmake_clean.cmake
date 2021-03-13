5f0a5fafb5b0f4776e44d2168e6b859496cff311
vald3nir <vald3nir@gmail.com>
2021-03-13 16:01:43 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/C_MQTT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_MQTT.dir/main.c.o"
+  "C_MQTT"
+  "C_MQTT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_MQTT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
